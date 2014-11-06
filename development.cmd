025099e5eb9c86ac9a3de5edb915289b6e474d0c
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2014-11-06 13:25:26 +0100

    Minor fix in package.json and development.cmd

--- development.cmd
+++ development.cmd
@@ -1,3 +1,3 @@
-start "IsItYourBirthday" cmd.exe /k ""
-start "IsItYourBirthday - development" cmd.exe /k "grunt dev"
-start "IsItYourBirthday - preview" cmd.exe /k "grunt prev"
+start "gibm-timetable" cmd.exe /k ""
+start "gibm-timetable - development" cmd.exe /k "grunt dev"
+start "gibm-timetable - preview" cmd.exe /k "grunt prev"
