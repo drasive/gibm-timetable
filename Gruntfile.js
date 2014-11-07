0a571f56ced174107717695dcf95354c73b5a32b
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2014-11-08 00:56:44 +0100

    Adapted Gruntfile.js to tasks in /build(/tasks)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,14 @@
 module.exports = function(grunt) {
   
+  // Load dependencies
+  var path = require('path');
+  
   // Load and execute grunt tasks
   require('load-grunt-config')(grunt, {
+    configPath: [
+	  path.join(process.cwd(), 'build/'),
+	  path.join(process.cwd(), 'build/tasks/')
+	],
     jitGrunt: true,
     data: {
       pkg: grunt.file.readJSON("package.json")
