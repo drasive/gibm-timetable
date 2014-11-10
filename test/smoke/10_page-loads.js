d214a408b626c71f6a7e8ae185b6809a159ffad4
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2014-11-10 18:46:16 +0100

    Made acceptance tests more robust

--- test/smoke/10_page-loads.js
+++ test/smoke/10_page-loads.js
@@ -2,9 +2,9 @@ module.exports = {
   "Smoke test - Page loads" : function (browser) {
     browser
       .url("http://localhost:8080/index.html")
-      .waitForElementVisible("body", 500)
+      .waitForElementVisible("body", 1000)
       
-      .waitForElementVisible(".content", 5000)
+      .waitForElementVisible(".content", 10000)
       
       .end();
   }
