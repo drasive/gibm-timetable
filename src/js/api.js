7aa87e9af7857938356a384d619dc9d24660982c
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2014-11-10 23:17:03 +0100

    Working on implementing API unit tests for getLessons()

--- src/js/api.js
+++ src/js/api.js
@@ -22,6 +22,16 @@ function getClasses(professionId) {
 }
 
 function getLessons(classId, week, year) {
+    if (typeof classId !== 'number' || !isInteger(classId)) {
+        return null;
+    }
+    if (typeof week !== "undefined" && week !== null && !isInteger(week)) {
+        return null;
+    }
+    if (typeof year !== "undefined" && year !== null && !isInteger(year)) {
+        return null;
+    }
+
     var weekParameter = week + '-' + year;;
 
     return $.ajax('http://home.gibm.ch/interfaces/133/tafel.php', {
--- test/unit/api.js
+++ test/unit/api.js
@@ -1,5 +1,6 @@
 module("API");
 
+// ========== getProfessions()
 QUnit.asyncTest( "Get all professions", function(assert) {
     apiTimeout = 10 * 1000;
     
@@ -17,14 +18,16 @@ QUnit.asyncTest( "Get all professions", function(assert) {
 });
 
 
+// ========== getClasses()
 QUnit.test( "Get classes general", function(assert) {
     assert.notStrictEqual(getClasses(), null, "undefined is not null");
     assert.notStrictEqual(getClasses(null), null, "null is not null");
-	assert.notStrictEqual(getClasses(10), null, "10 is not null");
-	
+    assert.notStrictEqual(getClasses(10), null, "10 is not null");
+    
     assert.strictEqual(getClasses(13.37), null, "13.37 is null");
     assert.strictEqual(getClasses("abc"), null, "'abc' is null");
 });
+
 
 QUnit.asyncTest( "Get all classes", function(assert) {
     apiTimeout = 10 * 1000;
@@ -75,3 +78,53 @@ QUnit.asyncTest( "Get classes for profession #15", function(assert) {
           start();
         });
 });
+
+
+// ========== getLessons()
+QUnit.test( "Get lessons general", function(assert) {
+    var classId = 2467010; // "IAP 14-18 B"
+    assert.strictEqual(getLessons(), null, "undefined is null");
+    assert.strictEqual(getLessons(null), null, "null is null");    
+    assert.strictEqual(getLessons(13.37), null, "13.37 is null");
+    assert.strictEqual(getLessons("abc"), null, "'abc' is null");
+	assert.notStrictEqual(getLessons(classId), null, classId + " is not null");
+	
+	// TODO: __
+});
+
+
+//QUnit.asyncTest( "Get lessons for class #2467010", function(assert) {
+//    apiTimeout = 10 * 1000;
+//    
+//    var classId = 2467010; // "IAP 14-18 B"
+//    getLessons(classId)
+//        .success(function (data) {
+//		  // No minimum amount of lessons (holidays etc.)
+//          assert.ok(data.length <= 100, "At most 100 (" + data.length + ") lessons were received for class #" + classId);		  
+//        })
+//        .error(function (xhr, status, error) {
+//          assert.ok(false, "Failed to retrieve lessons for class #" + classId + " (" + status + "): " + xhr.responseText);
+//        })
+//        .always(function () {
+//          start();
+//        });
+//});
+//
+//QUnit.asyncTest( "Get lessons for class #10", function(assert) {
+//    apiTimeout = 10 * 1000;
+//    
+//    var classId = 10; // TODO
+//	var week = 10;
+//	var year = 10;
+//    getLessons(classId, week, year)
+//        .success(function (data) {
+//          assert.ok(data.length >= 15, "At least 15 (" + data.length + ") classes were received for profession #" + professionId);
+//          assert.ok(data.length <= 100, "At most 100 (" + data.length + ") classes were received for profession #" + professionId);		  
+//        })
+//        .error(function (xhr, status, error) {
+//          assert.ok(false, "Failed to retrieve lessons for class #" + classId + " in week " + week + " - " + year + " (" + status + "): " + xhr.responseText);
+//        })
+//        .always(function () {
+//          start();
+//        });
+//});
